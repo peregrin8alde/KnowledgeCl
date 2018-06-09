fbbe7d4d1edbc023cbce7d34f46cef4c46fa2f30
peregrin8alde <37018345+peregrin8alde@users.noreply.github.com>
2018-06-09 12:56:44 +0900

    fixed #1

--- KnowledgeApiCl/run.sh
+++ KnowledgeApiCl/run.sh
@@ -4,8 +4,9 @@ URL=$1
 TOKEN=$2
 TITLE=$3
 TAGS=$4
+CONTENTFILE=$5
 
 java -cp target/KnowledgeApiCl-1.0-SNAPSHOT.jar:target/dependency/* \
-  myapps.App $URL $TOKEN $TITLE $TAGS
+  myapps.App $URL $TOKEN $TITLE $TAGS $CONTENTFILE
 
 exit 0;
--- KnowledgeApiCl/src/main/java/myapps/App.java
+++ KnowledgeApiCl/src/main/java/myapps/App.java
@@ -7,6 +7,11 @@ import java.util.Map;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.annotation.*;
@@ -34,18 +39,20 @@ public class App {
         String token = args[1];
         String titel = args[2];
         String tags = args[3];
+        String fileName = args[4];
 
         App app = new App(url, token);
-        app.postKnowledge(titel, tags);
+        app.postKnowledge(titel, tags, fileName);
     }
     
-    public void postKnowledge(String titel, String tags) {
+    public void postKnowledge(String titel, String tags, String fileName) {
         Knowledge knowledge = new Knowledge(titel);
         
         if (tags.length() != 0) {
             List<String> listTags = Arrays.asList(tags.split(","));
             knowledge.setTags(listTags);
         }
+        knowledge.setContent(readTextFileAll(fileName));
         
         ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
@@ -72,5 +79,15 @@ public class App {
         try (Response response = client.newCall(request).execute()) {
             return response.body().string();
         }
+    }
+    
+    public String readTextFileAll(String fileName) {
+        try (Stream<String> stream = Files.lines(Paths.get(fileName), StandardCharsets.UTF_8)) {
+            return stream.collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        return null;
     }
 }
