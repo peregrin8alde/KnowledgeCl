59e49f854e32619ff5f0d3876296ad9f4e7343a9
peregrin8alde <37018345+peregrin8alde@users.noreply.github.com>
2018-06-09 12:21:00 +0900

    fixed run.sh path

--- KnowledgeApiCl/test.sh
+++ KnowledgeApiCl/test.sh
@@ -5,6 +5,6 @@ TOKEN="F1TM3vOLMkHWDx0QJTzaBvltjZW6QAZwNVR0B3UnqF1IUrPfp4LM8SpadOUVlwWl"
 TITLE="title1"
 TAGS="api,/aaaa/bbb/ccc.md"
 
-../run.sh $URL $TOKEN $TITLE $TAGS
+./run.sh $URL $TOKEN $TITLE $TAGS
 
 exit 0;
