e9460a8e1a807f311abc2d9acd08fa17831ea5eb
peregrin8alde <37018345+peregrin8alde@users.noreply.github.com>
2018-06-09 18:55:03 +0900

    fix #3

---
+++ KnowledgeApiCl/testDir.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+
+URL="http://knowledge:8080"
+TOKEN="xKuWT1ArbcFrQR8PxIdUTf2Uk9AmmshMigc5Dq2a7JvsNMlDaAVcCr7xJNDN8n6y"
+BASEDIRNAME=/base
+CONTENTSDIR=..
+
+./runDir.sh $URL $TOKEN $BASEDIRNAME $CONTENTSDIR
+
+exit 0;
