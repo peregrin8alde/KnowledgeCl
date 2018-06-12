2259470b5eb7055c7b6b242a3cad0544d833084b
Okada Hayato <37018345+peregrin8alde@users.noreply.github.com>
2018-06-12 21:12:03 +0900

    スペースが含まれた場合におかしくなるのを修正

--- KnowledgeApiCl/runDir.sh
+++ KnowledgeApiCl/runDir.sh
@@ -6,25 +6,23 @@ BASEDIRNAME=$3
 CONTENTSDIR=$4
 PRVIVATE=$5
 
-find $CONTENTSDIR -name "*.md" | while read FILE
+find "$CONTENTSDIR" -name "*.md" | while read FILE
 do
   TITLE=`sed -n -E 's/^# (.*)/\1/p' $FILE | head -n 1`
   if [ -z "$TITLE" ]; then
-    TITLE=$(basename $FILE)
+    TITLE="$(basename $FILE)"
   fi
   
   TAGS="api,path:$BASEDIRNAME/${FILE#$CONTENTSDIR}"
-  CONTENTFILE=${FILE}
+  CONTENTFILE="${FILE}"
 
   ./run.sh -mode "POST" \
-           -url $URL \
-           -token $TOKEN \
-           -title $TITLE \
-           -tags $TAGS \
+           -url "$URL" \
+           -token "$TOKEN" \
+           -title "$TITLE" \
+           -tags "$TAGS" \
            $PRVIVATE \
-           -file $CONTENTFILE
+           -file "$CONTENTFILE"
 done
-
-
 
 exit 0;
